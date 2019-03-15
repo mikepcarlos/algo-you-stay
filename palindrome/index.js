da93ce83c3645c4c181f7ba2561c1008e2d70a3d
mikepcarlos <michaelpcarlos@gmail.com>
2019-03-15 16:36:56 -0400

    palindromes

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// solution 1: (My Solution)
+
+// function palindrome(str) {
+//   return str.split("").reverse().join("") === str;
+// }
+
+// solution 2: (Alternate Solution)
+
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  })
+}
 
 module.exports = palindrome;
